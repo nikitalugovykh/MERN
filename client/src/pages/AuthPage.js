c5e81ebd30efc1312f7567400f9697bec3860362
Nikita Lugovykh <belikovannn@mail.ru>
2021-08-27 15:56:09 +0200

    add missing files

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AuthPage = () => {
+    return (
+    <div>
+        <h1>Auth page</h1>
+    </div>
+    )
+}
---
+++ client/src/routes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { LinksPage } from './pages/LinksPage.js';
+import { CreatePage } from './pages/CreatePage.js';
+import { DetailPage } from './pages/DetailPage.js';
+import { AuthPage } from './pages/AuthPage';
+
+export const useRoutes = isAuthenticated => {
+    if (isAuthenticated) {
+        return (
+           <Switch>
+               <Route path="/link" exact>
+                   <LinksPage />
+               </Route>
+               <Route path="/create">
+                   <CreatePage/>
+               </Route>
+               <Route path="/detail/:id">
+                   <DetailPage/>
+               </Route>
+               <Redirect to="/create"/>
+           </Switch>
+        )
+    }
+
+    return (
+        <Switch>
+            <Route path="/" exact>
+                <AuthPage/>
+            </Route>
+            <Redirect to="/"/>
+        </Switch>
+    )
+}
