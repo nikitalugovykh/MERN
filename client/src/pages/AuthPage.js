c3b93f32019496415df196f711968fcd0b6b9ef8
Nikita Lugovykh <belikovannn@mail.ru>
2021-08-27 16:43:23 +0200

    add AuthPage.css, update style of AuthPage component, add validation

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import'./AuthPage.css';
 
 export const AuthPage = () => {
+    const [form, setForm] = useState({
+        email: '', 
+        password: ''
+    })
+
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
     return (
-    <div>
-        <h1>Auth page</h1>
+    <div className="row">
+        <div className="col s6 offset-s3">
+            <h1>Сократи ссылку</h1>
+            <div className="card blue darken-1">
+                <div className="card-content white-text">
+                    <span className="card-title">Авторизация</span>
+                </div>
+                
+                <div className="input-field">
+                    <input 
+                        placeholder="Введите email" 
+                        id="email" 
+                        type="text" 
+                        class="validate"
+                        name="email"
+                        className="yellow-input"
+                        onchange = {changeHandler}
+                    />
+                    <label htmlFor="email">Email</label>
+                </div>
+
+                <div className="input-field ">
+                    <input 
+                        placeholder="Введите логин" 
+                        id="password" 
+                        type="password" 
+                        class="validate"
+                        name="password"
+                        className="yellow-input"
+                        onchange = {changeHandler}
+                    />
+                    <label htmlFor="email">Пароль</label>
+                </div>
+                
+                <div className="card-action">
+                    <button className="btn yellow darken-4 btn_logIn">Войти</button>
+                    <button className="btn grey darken-1">Регистрация</button>
+                </div>
+            </div>
+        </div>
     </div>
     )
 }
