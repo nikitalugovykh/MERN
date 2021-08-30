94e80296efc68c2d40f049c86d15afef9a142aca
Nikita Lugovykh <belikovannn@mail.ru>
2021-08-30 12:49:08 +0200

    update backend link route

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,11 +1,33 @@
 const { Router } = require('express');
 const Link = require('./../models/Link');
-const auth = require('./../middleware/auth.middleware.js')
+const auth = require('./../middleware/auth.middleware.js');
+const config = require('config');
+const shortid = require('shortid');
 const router = Router();
 
-router.post ('/generate', async (req, res) => {
+router.post ('/generate', auth, async (req, res) => {
     
     try {
+        const baseUrl = config.get('baseUrl');
+        const { from } = req.body;
+
+        const code = shortid.generate();
+
+        const existing = await Link.findOne({ from });
+
+        if (existing) {
+            return res.json({ link: existing })
+        }
+
+        const to = baseUrl + '/t' + code;
+
+        const link = new Link ({
+            code,to, from, owner: req.user.userId
+        })
+
+        await link.save()
+
+        res.status(201),json({ link })
 
     } catch(e) {
         res.status(500).json({message: 'Что-то пошло не так, попробуйте снова!'})
