6f93c31d55fcb2a9260b47b35cd711983d425ab4
Nikita Lugovykh <belikovannn@mail.ru>
2021-08-30 12:23:17 +0200

    add  middleware for link route which return middle decoded token

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const Link = require('./../models/Link');
+const auth = require('./../middleware/auth.middleware.js')
 const router = Router();
 
 router.post ('/generate', async (req, res) => {
@@ -12,10 +13,11 @@ router.post ('/generate', async (req, res) => {
 
 });
 
-router.get ('/', async (req, res) => {
+router.get ('/', auth, async (req, res) => {
 
     try {
-
+        const links = await Link.find({ owner: req.user.id })
+        res.json(links)
     } catch(e) {
         res.status(500).json({message: 'Что-то пошло не так, попробуйте снова!'})
     }
@@ -25,11 +27,12 @@ router.get ('/', async (req, res) => {
 router.get ('/:id', async (req, res) => {
 
     try {
-
+        const link = await Link.findById(req.params.id)
+        res.json(link)
     } catch(e) {
         res.status(500).json({message: 'Что-то пошло не так, попробуйте снова!'})
     }
-    
+
 });
 
 module.exports = router;
